aa649911e79e730aec695267d9927b0a8a0e582f
Kenneth Sundqvist <eviloatmeal@gmail.com>
2012-12-17 22:03:31 +0100

    Add Object.create for future reference/experiments

---
+++ js/Object.create.js
@@ -0,0 +1,12 @@
+/*
+    "Prototypal Inheritance in JavaScript" by Douglas Crockford.
+    http://javascript.crockford.com/prototypal.html
+*/
+
+if (typeof Object.create !== 'function') {
+    Object.create = function (o) {
+        function F() {};
+        F.prototype = o;
+        return new F();
+    };
+}
